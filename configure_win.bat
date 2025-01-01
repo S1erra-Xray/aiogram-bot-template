e71eddea58eaf9b362f5009c4d0d9b566559e958
S1erra-Xray <none@gmail.com>
2025-01-02 03:45:12 +0400

    added config scripts for linux and windows setup

---
+++ configure_win.bat
@@ -0,0 +1,8 @@
+set PROJECT_DIR=.
+
+poetry run python -m ruff check %PROJECT_DIR% --config pyproject.toml --fix
+poetry run python -m isort %PROJECT_DIR%
+poetry run python -m mypy %PROJECT_DIR% --config-file pyproject.toml
+poetry run python -m black %PROJECT_DIR% --config pyproject.toml
+
+poetry run python infra/scripts/init_project.py
